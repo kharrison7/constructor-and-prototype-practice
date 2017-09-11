09666a8c06630cac4587ad316b5caa4cd180a388
Kevin Harrison <kharrison7@elon.edu>
2017-09-11 11:38:32 -0400

    THis makes all the tests turn green.

--- starter_files/main.js
+++ starter_files/main.js
@@ -8,18 +8,19 @@
 //            /____/
 
 // Dog Constructor & Prototype
-function Dog () {
+function Dog(name, status, color) {
   this.name = name;
   this.status = status;
-
+  this.color = color;
+  this.hungry = true;
 };
 
 // Instances of Dog
 // Needed: sadie, moonshine, atticus
 
-let sadie = new Dog();
-let moonshine = new Dog();
-let atticus = new Dog();
+let sadie = new Dog('sadie', 'normal', 'black');
+let moonshine = new Dog('moonshine', 'normal', 'grey');
+let atticus = new Dog('atticus', 'normal', 'white');
 
 
 //     __
@@ -29,15 +30,21 @@ let atticus = new Dog();
 // /_/ /_/\__,_/_/ /_/ /_/\__,_/_/ /_/____/
 
 // Human Constructor & Prototype
-function Human (pet) {
- this.pet = function(){
-  console.log(this.name);
-};
-
+function Human(cool) {
+  this.cool = cool;
+  this.pet = function(x) {
+    return x.status = "happy";
+  };
+  this.feed = function(x) {
+    return x.hungry = false;
+  };
 };
 
 // Instances of Human
 // Needed: mason, julia
 
-let mason = new Human();
-let julia = new Human();
+let mason = new Human(false);
+let julia = new Human(true);
+
+// Feeding sadie makes her hunger false.
+julia.feed(sadie);
